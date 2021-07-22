cd58973ab12da40d0d9c249b8571540732b90967
MSIHub <mohamedsadiq.ikbal@gmail.com>
2021-07-22 22:32:31 +0200

    Door Trigger added, progrids included

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject door;
+
+
+
+}
