089ee8b291c1ef75b49bd0f0cb7c98b9b56f21d0
MSIHub <mohamedsadiq.ikbal@gmail.com>
2021-07-23 02:26:30 +0200

    Modified the enemy controller to move to all the points under the path game object with component "PointinPath"

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Serialization;
@@ -8,21 +9,31 @@ public class EnemyController : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent agent;
     //Path
-    [SerializeField] private Transform point1;
-    [SerializeField] private Transform point2;
-
+    [SerializeField] private Transform path;
     //Tolerance between target and agent
     [SerializeField] private float threshold = 0.5f;
     
     private bool _moving = false;
     private Transform _currentPoint;
-    
-    
+
+    private List<Transform> _robotPath = new List<Transform>(); // List to store all the child object of Path
+    private int itr = 0; // Iterator for the robot path points
+    private int _pathSize = 0;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _pathSize = path.childCount; //Number of points in the Path object
+        //Extracting each child objects
+        foreach (Transform child in path) 
+        {
+            //if the child object does not have the PointinPath component then its not a path
+            //Double layer of protection
+            if (child.GetComponent<PointInPath>())
+            {
+                _robotPath.Add(child);    
+            }
+        }
     }
 
     // Update is called once per frame
@@ -30,13 +41,14 @@ public class EnemyController : MonoBehaviour
     {
         if (!_moving)
         {
-            if (_currentPoint == point1)
+            if (itr < _pathSize)
             {
-                _currentPoint = point2;
+                _currentPoint = _robotPath[itr];
+                itr++;
             }
             else
             {
-                _currentPoint = point1;
+                itr = 0;
             }
             agent.SetDestination(_currentPoint.position);
             _moving = true;
