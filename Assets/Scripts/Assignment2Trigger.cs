48e5ee471e017530652d8984b824507d5558bb9a
Mohamed sadiq ikbal <mohamedsadiq.ikbal@gmail.com>
2021-07-23 20:01:33 +0200

    Added UI Canvas with pannel to display the message. Added the effects from the cat assest. Disabled minimap canvas. #TODO: Handle mutliple canvas to show and hide by grouping.

--- Assets/Scripts/Assignment2Trigger.cs
+++ Assets/Scripts/Assignment2Trigger.cs
@@ -5,23 +5,35 @@ using UnityEngine;
 
 public class Assignment2Trigger : MonoBehaviour
 {
-  [SerializeField] private GameObject textField;
+  [SerializeField] private Canvas canvasDisplay;
+  [SerializeField] private Canvas canvasHide;
   
   // Start is called before the first frame update
   void Start()
   {
-      textField.SetActive(false);
+      CloseAllCanvas();
   }
   
   private void OnTriggerEnter(Collider other)
   {
      Debug.Log(message:"Congrats, you triggered the volume!");
-     textField.SetActive(true);
+     ActivateCanvas();
   }
   private void OnTriggerExit(Collider other)
   {
       Debug.Log(message:"You are out of trigger volume");
-      textField.SetActive(false);
+      CloseAllCanvas();
   }
 
+  private void ActivateCanvas()
+  {
+      canvasHide.gameObject.SetActive(false);
+      canvasDisplay.gameObject.SetActive(true);
+  }
+
+  private void CloseAllCanvas()
+  {
+      canvasHide.gameObject.SetActive(true);
+      canvasDisplay.gameObject.SetActive(false);
+  }
 }
