c41cbc2a4e98e375d4b2484ebf129515925dc907
Mohamed sadiq ikbal <mohamedsadiq.ikbal@gmail.com>
2021-07-24 01:37:26 +0200

    Added the Timer in update function and on trigger started the timer. Deltatime works only in update function as it is called at each deltatime

--- Assets/Scripts/Assignment2Trigger.cs
+++ Assets/Scripts/Assignment2Trigger.cs
@@ -1,17 +1,64 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
+using StarterAssets;
 using UnityEngine;
 
 public class Assignment2Trigger : MonoBehaviour
 {
+  [SerializeField] private GameObject obj;
+  [SerializeField]private float _waitTimeInSeconds = 10.0f;
+  
+  private float _timer = 0.0f;
+  private bool _timerStarted = false;
+  private Vector3 _objInitialPosition;
+  private Quaternion _objInitialRotation;
+  private Vector3 _objInitialScale;
+  private void Start()
+  {
+    obj.SetActive(false);
+    _objInitialPosition = obj.transform.localPosition;
+    _objInitialRotation = obj.transform.localRotation;
+    _objInitialScale = obj.transform.localScale;
+
+  }
+
+  private void Update()
+  {
+    if (_timerStarted)
+    {
+      _timer += Time.deltaTime;
+    }
+    
+    if (_timer > _waitTimeInSeconds)
+    {
+      obj.SetActive(true);
+      _timerStarted = false;
+    }
+  }
+
   private void OnTriggerEnter(Collider other)
   {
-    Debug.Log(message:"Congrats, you triggered the volume!");
+    if (other.GetComponent<FirstPersonController>())
+    {
+      if (!_timerStarted)
+      {
+        _timerStarted = true;
+      } 
+    }
   }
   
   private void OnTriggerExit(Collider other)
   {
-    Debug.Log(message:"You are out of trigger volume");
+    if (other.GetComponent<FirstPersonController>())
+    {
+      obj.transform.localPosition = _objInitialPosition;
+      obj.transform.localRotation = _objInitialRotation;
+      obj.transform.localScale = _objInitialScale;
+      obj.SetActive(false);
+      _timerStarted = false;
+      _timer = 0.0f;
+    }
   }
 }
