165c143304912d1bdef308532ddc396291eda464
MSIHub <mohamedsadiq.ikbal@gmail.com>
2021-07-31 01:48:53 +0200

    Implemented the modulo operator for the loop and pingpong patroltype

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -14,17 +14,16 @@ public class EnemyController : MonoBehaviour
     
     private bool _moving = false;
     private Transform _currentPoint;
+
     private int _routeIndex = 0;
-    private bool _forwardAlongPath = true;
     private int _routeCount = 0;
+    private bool _forwardAlongPath = true;
+    private bool _firstIteration = true;
     
     // Start is called before the first frame update
     void Start()
     {
         _currentPoint = _patrolRoute.route[_routeIndex];
-        
-        
-
     }
 
     // Update is called once per frame
@@ -37,12 +36,10 @@ public class EnemyController : MonoBehaviour
             _moving = true;
         }
 
-        
         if (_moving &&  Vector3.Distance(transform.position, _currentPoint.position)< _threshold)
         {
             _moving = false;
         }
-
     }
 
     private void NextPatrolPoint()
@@ -64,41 +61,47 @@ public class EnemyController : MonoBehaviour
             //Reverse the direction when it reaches the last point 
             if (!_forwardAlongPath | _routeIndex == _routeCount - 1)
             {
+                _forwardAlongPath = false;
+                if (_firstIteration)
+                {
+                    _firstIteration = false; // to ensure that the last point is visited before returning
+                    return;
+                }
+                _routeIndex = (((_routeIndex - 1) % _routeCount) +_routeCount) % _routeCount ;
                 if (_routeIndex == 0)
                 {
-                    _forwardAlongPath = true;                    
+                    _forwardAlongPath = true;
+                    _firstIteration = true;
                 }
-                _forwardAlongPath = false;
-                _routeIndex = (((_routeIndex - 1) % _routeCount) +_routeCount) % _routeCount ;
             }
         }
-
-        /*
-        if (_forwardAlongPath)
-        {
-            _routeIndex++;    
-        }
-        else
-        {
-            _routeIndex--;
-        }
-        
-        if (_routeIndex == 0)
-        {
-            _forwardAlongPath = true;
-        }
-
-        if (_routeIndex == _patrolRoute.route.Count)
-        {
-            if (_patrolRoute.patrolType == PatrolRoute.PatrolType.Loop)
-            {
-                _routeIndex = 0;    
-            }
-            else if (_patrolRoute.patrolType == PatrolRoute.PatrolType.PingPong)
-            {
-                _forwardAlongPath = false;
-                _routeIndex--;
-            }
-        }*/
     }
 }
+
+/*
+if (_forwardAlongPath)
+{
+    _routeIndex++;    
+}
+else
+{
+    _routeIndex--;
+}
+
+if (_routeIndex == 0)
+{
+    _forwardAlongPath = true;
+}
+
+if (_routeIndex == _patrolRoute.route.Count)
+{
+    if (_patrolRoute.patrolType == PatrolRoute.PatrolType.Loop)
+    {
+        _routeIndex = 0;    
+    }
+    else if (_patrolRoute.patrolType == PatrolRoute.PatrolType.PingPong)
+    {
+        _forwardAlongPath = false;
+        _routeIndex--;
+    }
+}*/
