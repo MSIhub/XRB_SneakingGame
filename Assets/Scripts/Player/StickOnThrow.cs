98945c8ce0b18d82f8009229631990eb8bbba3ab
MSIHub <mohamedsadiq.ikbal@gmail.com>
2021-08-02 12:53:02 +0200

    RayCast check in progress

--- Assets/Scripts/Player/StickOnThrow.cs
+++ Assets/Scripts/Player/StickOnThrow.cs
@@ -7,11 +7,32 @@ using UnityEngine;
 public class StickOnThrow : MonoBehaviour
 {
     public bool triggerSticking = false;
+
+    [SerializeField] private float _bombRange = 5.0f;
+    private bool _stuckToWall = false;
+
+    private void FixedUpdate()
+    {
+        if (_stuckToWall)
+        {
+            if (Physics.Raycast(transform.position, Vector3.forward, _bombRange))
+            {
+                Debug.Log("detected object");
+            }
+        }
+    }
+
     private void OnCollisionEnter(Collision other)
+    {
+        StickToCollidedSurfaceOnThrow();
+    }
+
+    private void StickToCollidedSurfaceOnThrow()
     {
         if (triggerSticking)
         {
-            transform.gameObject.GetComponent<Rigidbody>().isKinematic = true;//removes the physics
+            transform.gameObject.GetComponent<Rigidbody>().isKinematic = true; //removes the physics
+            _stuckToWall = true;
         }
     }
 }
