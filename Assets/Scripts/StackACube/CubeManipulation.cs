138af784a0963af8f788a554f564de2fdf055461
Mohamed Sadiq Ikbal <mohamedsadiq.ikbal@gmail.com>
2021-08-30 23:51:01 +0200

    Scaling and descaling done using button

--- Assets/Scripts/StackACube/CubeManipulation.cs
+++ Assets/Scripts/StackACube/CubeManipulation.cs
@@ -1,45 +1,69 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 namespace StackACube
 {
     public class CubeManipulation : MonoBehaviour
     {
+        [SerializeField] private GameObject _instantiateMesh;
         [SerializeField] private InputActionReference _leftPrimaryButtonReference;
         [SerializeField] private InputActionReference _rightPrimaryButtonReference;
-        [SerializeField] private GameObject _instantiateMesh;
+        [SerializeField] private InputActionReference _rightSecondaryButtonReference;
         [SerializeField] private InputActionReference _leftPosition;
         [SerializeField] private InputActionReference _leftRotation;
-        
 
-        private Vector3 _leftControllerPositon;
+        private Vector3 _leftControllerPosition;
         private Quaternion _leftControllerRotation;
-        private GameObject _spawnMesh;
+        private XRGrabInteractable _grabMesh;
         
         // Start is called before the first frame update
         void Start()
         {
-            
             _leftPrimaryButtonReference.action.performed += OnLeftPrimaryButtonPressed;
-            _rightPrimaryButtonReference.action.performed += OnRightPrimaryButtonPressed;
 
         }
+        
         private void OnLeftPrimaryButtonPressed(InputAction.CallbackContext obj)
         {
-            _spawnMesh = Instantiate(_instantiateMesh, _leftControllerPositon, _leftControllerRotation);
+            Instantiate(_instantiateMesh, _leftControllerPosition, _leftControllerRotation);
         }
+
+
+        public void EnableScaleMesh(XRGrabInteractable grabMesh)
+        {
+            _grabMesh = grabMesh;
+            _rightPrimaryButtonReference.action.performed += OnRightPrimaryButtonPressed;
+            _rightSecondaryButtonReference.action.performed += OnRightSecondaryButtonPressed;
+
+        }
+
+        public void DisableScaleMesh(XRGrabInteractable grabMesh)
+        {
+            _rightPrimaryButtonReference.action.performed -= OnRightPrimaryButtonPressed;
+            _rightSecondaryButtonReference.action.performed -= OnRightSecondaryButtonPressed;
+        }
+
+        
         private void OnRightPrimaryButtonPressed(InputAction.CallbackContext obj)
         {
-            Debug.Log("Right ButtonPressed");
-            _spawnMesh.transform.localScale += new Vector3(0f, 0.2f, 0f);
+            _grabMesh.gameObject.transform.localScale += Vector3.one*0.01f;
         }
+        
+        private void OnRightSecondaryButtonPressed(InputAction.CallbackContext obj)
+        {
+            _grabMesh.gameObject.transform.localScale -= Vector3.one*0.01f;
+        }
+
+
 
       
 
         // Update is called once per frame
         void Update()
         {
-            _leftControllerPositon = transform.TransformPoint(_leftPosition.action.ReadValue<Vector3>()) ;
+            _leftControllerPosition = transform.TransformPoint(_leftPosition.action.ReadValue<Vector3>()) ;
             _leftControllerRotation = _leftRotation.action.ReadValue<Quaternion>();//rotation is in body frame so no need to transform it
         }
     }
---
+++ Assets/Scripts/StackACube/ScalingFeature.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace StackACube
+{
+    public class ScalingFeature : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        public void PrintLog()
+        {
+            Debug.Log("Cube Scaling on");
+        }
+    }
+}
