2c73a69ae0dc59b1a024d44d1a61dcfa1f90c03c
Mohamed Sadiq Ikbal <mohamedsadiq.ikbal@gmail.com>
2021-08-30 17:38:59 +0200

    Cube positioning solved with a basic scaling system made

--- Assets/Scripts/StackACube/CubeManipulation.cs
+++ Assets/Scripts/StackACube/CubeManipulation.cs
@@ -14,6 +14,7 @@ namespace StackACube
 
         private Vector3 _leftControllerPositon;
         private Quaternion _leftControllerRotation;
+        private GameObject _spawnMesh;
         
         // Start is called before the first frame update
         void Start()
@@ -25,12 +26,12 @@ namespace StackACube
         }
         private void OnLeftPrimaryButtonPressed(InputAction.CallbackContext obj)
         {
-            Debug.Log("Left ButtonPressed");
-           Instantiate(_instantiateMesh, _leftControllerPositon, _leftControllerRotation);
+            _spawnMesh = Instantiate(_instantiateMesh, _leftControllerPositon, _leftControllerRotation);
         }
         private void OnRightPrimaryButtonPressed(InputAction.CallbackContext obj)
         {
             Debug.Log("Right ButtonPressed");
+            _spawnMesh.transform.localScale += new Vector3(0f, 0.2f, 0f);
         }
 
       
@@ -38,9 +39,8 @@ namespace StackACube
         // Update is called once per frame
         void Update()
         {
-            _leftControllerPositon = transform.position + _leftPosition.action.ReadValue<Vector3>() ;
-            _leftControllerRotation = _leftRotation.action.ReadValue<Quaternion>();
-            Debug.Log(_leftControllerPositon);
+            _leftControllerPositon = transform.TransformPoint(_leftPosition.action.ReadValue<Vector3>()) ;
+            _leftControllerRotation = _leftRotation.action.ReadValue<Quaternion>();//rotation is in body frame so no need to transform it
         }
     }
 }
