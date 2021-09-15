fb6b218958ce0537e781a741acec7f7c0053ba18
Mohamed Sadiq Ikbal <mohamedsadiq.ikbal@gmail.com>
2021-09-15 13:45:50 +0200

    Solved the hand hider issue.

--- Assets/Scripts/Hands/XRHandHider.cs
+++ Assets/Scripts/Hands/XRHandHider.cs
@@ -18,13 +18,14 @@ namespace Hands
             _controller.selectEntered.AddListener(SelectEntered);
             _controller.selectExited.AddListener(SelectExited);
 
-            _originalHandRot = _handRigidBody.transform.localRotation;//
+            //_originalHandRot = _handRigidBody.transform.localRotation;//
+            _originalHandRot = _configJoint.transform.localRotation;//
         }
         private void SelectEntered(SelectEnterEventArgs arg0)
         {
             if (arg0.interactable is BaseTeleportationInteractable) return;//is type comparision
             _configJoint.gameObject.SetActive(false);
-            _handRigidBody.gameObject.SetActive(false);
+            //_handRigidBody.gameObject.SetActive(false);
             _configJoint.connectedBody = null;
             CancelInvoke(nameof(ShowHand));
             
@@ -38,10 +39,12 @@ namespace Hands
         private void ShowHand()
         {
             _configJoint.gameObject.SetActive(true);
-            _handRigidBody.gameObject.SetActive(true);
-            _handRigidBody.transform.position = _controller.transform.position;//
-            _handRigidBody.transform.rotation = Quaternion.Euler(_controller.transform.eulerAngles + _originalHandRot.eulerAngles);//
-            _configJoint.connectedBody = _handRigidBody;
+           //_handRigidBody.gameObject.SetActive(true);
+           //_handRigidBody.transform.position = _controller.transform.position;//
+           //_handRigidBody.transform.rotation = Quaternion.Euler(_controller.transform.eulerAngles + _originalHandRot.eulerAngles);//
+           _configJoint.transform.position = _controller.transform.position;//
+           _configJoint.transform.rotation = Quaternion.Euler(_controller.transform.eulerAngles + _originalHandRot.eulerAngles);
+           _configJoint.connectedBody = _handRigidBody;
         }
         
     }
